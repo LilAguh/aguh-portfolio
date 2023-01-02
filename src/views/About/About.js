06ad178f3af5b6f0452f42362f37d27fbf8eb94e
LilAguh <aguezeochoa@gmail.com>
2023-01-01 22:19:30 -0300

    new folder where views are saved

---
+++ src/views/About/About.js
@@ -0,0 +1,21 @@
+import "./About.css"
+
+export default function About() {
+    return (
+        <div className="About">
+            <div className="AboutTitle">
+                <h2 className="AboutTitleClass">FRONTEND</h2>
+                <h2 className="AboutTitleClass">DEVELOPER</h2>
+            </div>
+            <div className="AboutInformation">
+                <p className="AboutInformationText">
+                    Hi, I'm agustin ochoa, aguh, mern fullstack developer.
+                    Graduated from the Mindhub bootcamp in 2022, I like the frontend and the design, wanting to specialize in mobile applications.
+                </p>
+                <p className="AboutInformationText">
+                    Argentinian and food lover
+                </p>
+            </div>
+        </div>
+    )
+}
---
+++ src/views/ContactMe/ContactMe.js
@@ -0,0 +1,9 @@
+import "./ContactMe.css"
+
+export default function ContactMe() {
+    return (
+        <div className="ContactMe">
+            <p>hola</p>
+        </div>
+    )
+}
