517793d96a83501ac55e285f2198a920d29af91e
LilAguh <aguezeochoa@gmail.com>
2023-01-02 23:32:45 -0300

    add smoothing efect in buttons of navbar

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,3 +1,4 @@
+import { Link, animateScroll as scroll } from "react-scroll";
 import "./Nav.css"
 
 export default function Nav() {
@@ -9,8 +10,27 @@ export default function Nav() {
             </div>
             <nav>
                 <ul className="navContainer">
-                    <li className="navLink">About</li>
-                    <li className="navLink">Works</li>
+                    <li className="navLink">
+                        <Link
+                            activeClass="active"
+                            to="section1"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                        >About</Link>
+                    </li>
+                    <li className="navLink">
+                        <Link
+                            activeClass="active"
+                            to="section2"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                        >
+                            Works
+                        </Link></li>
                     <li className="navLink">Contact</li>
                 </ul>
             </nav>
