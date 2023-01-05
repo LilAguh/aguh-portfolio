dee129b310c9016f086c70a6e5c0b33527e3c1ae
LilAguh <aguezeochoa@gmail.com>
2023-01-04 23:15:54 -0300

    modification in app files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import Nav from './components/nav/Nav';
-import About from './views/About/About';
-import ContactMe from './views/ContactMe/ContactMe'
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Nav from "./Components/nav/Nav";
+import Section from "./Components/Section";
+import dummyText from "./DummyText";
+import About from "./views/About/About";
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <About />
-      <ContactMe />
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <About
+          id="about"
+        />
+        <Section
+          title="Section 2"
+          subtitle={dummyText}
+          dark={false}
+          id="section2"
+        />
+        <Section
+          title="Section 3"
+          subtitle={dummyText}
+          dark={true}
+          id="section3"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
