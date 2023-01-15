1f0604d3fd5d7500092d0fb444eba6949b0612c5
LilAguh <aguezeochoa@gmail.com>
2023-01-14 22:39:32 -0300

    change conten of about to welcome

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -1,22 +1,21 @@
 import "./About.css"
-import Angel from "../../assets/images/angel1.png"
 
 export default function About() {
     return (
         <div className="About" id="about">
             <div className="AboutTitle">
-                <h2 className="AboutTitleClass">FRONTEND</h2>
-                <h2 className="AboutTitleClass">DEVELOPER</h2>
+                {/* <h2 className="AboutTitleClass">AGUSTIN</h2>
+                <h2 className="AboutTitleClass">OCHOA</h2> */}
             </div>
             <div className="AboutInformation">
-                <img src={Angel} className="Angel" />
-                {/* <p className="AboutInformationText">
+                {/* <Angel /> */}
+                <p className="AboutInformationText">
                     Hi, I'm agustin ochoa, aguh, mern fullstack developer.
                     Graduated from the Mindhub bootcamp in 2022, I like the frontend and the design, wanting to specialize in mobile applications.
                 </p>
                 <p className="AboutInformationText">
                     Argentinian and food lover
-                </p> */}
+                </p>
             </div>
         </div>
     )
