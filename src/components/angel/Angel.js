7c8d47db23522152197041c13a99909d0da36134
LilAguh <aguezeochoa@gmail.com>
2023-01-14 22:42:54 -0300

    new angel component

---
+++ src/components/angel/Angel.js
@@ -0,0 +1,50 @@
+import "./Angel.css"
+import Angelimg from "../../assets/images/angel1.png"
+
+export default function Angel() {
+    const i1 = { "--i": 1 }
+    const i2 = { "--i": 2 }
+    const i3 = { "--i": 3 }
+    const i4 = { "--i": 4 }
+    const i5 = { "--i": 5 }
+    const i6 = { "--i": 6 }
+    const i7 = { "--i": 7 }
+    const i8 = { "--i": 8 }
+    const i9 = { "--i": 9 }
+    const i10 = { "--i": 10 }
+    const i11 = { "--i": 11 }
+    const i12 = { "--i": 12 }
+    const i13 = { "--i": 13 }
+    const i14 = { "--i": 14 }
+    const i15 = { "--i": 15 }
+    const i16 = { "--i": 16 }
+    const i17 = { "--i": 17 }
+    const i18 = { "--i": 18 }
+    const i19 = { "--i": 19 }
+    return (
+        <div className="Angel">
+            <img src={Angelimg} className="AngelImg" />
+            <div className="AngelCircle">
+                <spam style={i1}>f</spam>
+                <spam style={i2}>r</spam>
+                <spam style={i3}>o</spam>
+                <spam style={i4}>n</spam>
+                <spam style={i5}>t</spam>
+                <spam style={i6}>e</spam>
+                <spam style={i7}>n</spam>
+                <spam style={i8}>d</spam>
+                <spam style={i9}>‎</spam>
+                <spam style={i10}>d</spam>
+                <spam style={i11}>e</spam>
+                <spam style={i12}>v</spam>
+                <spam style={i13}>e</spam>
+                <spam style={i14}>l</spam>
+                <spam style={i15}>o</spam>
+                <spam style={i16}>p</spam>
+                <spam style={i17}>e</spam>
+                <spam style={i18}>r</spam>
+                <spam style={i19}>‎</spam>
+            </div>
+        </div>
+    )
+}
