90157274c76a21c2de604e9f9e43474bdd7b8e25
LilAguh <aguezeochoa@gmail.com>
2023-01-15 23:14:26 -0300

    new view for works

---
+++ src/views/Works/Works.js
@@ -0,0 +1,10 @@
+import "./Works.css"
+import AngelTRacker from "../../Components/AngelTracker/AngelTracker"
+
+export default function Works() {
+    return (
+        <div className="Works" id="works">
+            <AngelTRacker />
+        </div>
+    )
+}
